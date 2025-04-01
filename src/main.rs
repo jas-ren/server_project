5aca7ca9055de5b7460870aab236d3638eb1f592
jas-ren <78553107+jas-ren@users.noreply.github.com>
2025-04-01 02:05:03 -0400

    WIP create new process for DELETE

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,48 @@ use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::env;
 use std::os::unix::fs::OpenOptionsExt;
 use fs2::FileExt;
-use std::thread;
-use std::time;
+use nix::unistd;
 
 const DB_FILE_PATH: &str = "db.txt";
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let function: &str = &args[1];
-    let target_key: &str = &args[2];
+    
 
     match function.to_uppercase().as_str() {
         "GET" => {
+            let target_key: &str = &args[2];
             match get_by_key(target_key)? {
                 Some(value) => println!("{value}"),
                 None => println!("Key {target_key} not found"),
             }
         },
         "SET" => {
+            let target_key: &str = &args[2];
             let target_value: &str = &args[3];
             set_value(target_key, target_value)?;
             println!("Key {target_key} set to {target_value}");
         },
+        "DELETE" => {
+            let target_keys = &args[2..];
+            let mut child_pids = Vec::new();
+
+
+            for key in target_keys {
+                match unsafe {
+                    nix::unistd::fork()
+                } {
+                    Ok(nix::unistd::ForkResult::Parent {child} ) => {
+                        child_pids.push(child);
+                    },
+                    Ok(nix::unistd::ForkResult::Child) => {
+                        todo!()
+                    }
+                }
+                let new_proc = std::process::Command::new(delete_by_key(key));
+            }
+        }
         _ => {
             println!("invalid function. Please use GET or SET\n");
             return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid function"));
@@ -92,5 +112,41 @@ fn set_value(target_key: &str, target_value: &str) -> std::io::Result<Option<Str
     drop(bufwriter);
     FileExt::unlock(&file)?;
     Ok(None)
+
+}
+
+
+fn delete_by_key(target_key: &str) -> std::io::Result<Option<String>> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .open(DB_FILE_PATH)?;
+    FileExt::lock_exclusive(&file)?;
+    let bufreader = BufReader::new(&file);
+
+    let mut lines = Vec::new();
+    let mut deleted_key: Option<String> = None;
+    for line_result in bufreader.lines() {
+        let line = line_result?;
+        let mut parts = line.splitn(2, " ");
+        if let (Some(key), Some(val)) = (parts.next(), parts.next()) {
+            if key != target_key {
+                lines.push(line);
+            } else {
+                deleted_key = Some(val.to_string())
+            }
+        }
+    }
+
+    let mut bufwriter = BufWriter::new(&mut file);
+    for line in lines {
+        writeln!(bufwriter, "{line}")?;
+    }
+    bufwriter.flush()?;
+    drop(bufwriter);
+    FileExt::unlock(&file)?;
+
+    Ok(deleted_key)
 
 }
